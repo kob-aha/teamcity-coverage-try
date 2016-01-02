1988a918d9c2bbd61f3d5373ed18d5b723372531
Koby Aharon <kobyahron@gmail.com>
2016-01-02 19:14:16 +0200

    Add excluded file to check functionality

---
+++ src/main/java/org/edu/ka/ExcludedService.java
@@ -0,0 +1,10 @@
+package org.edu.ka;
+
+/**
+ * Created by koby on 1/1/16.
+ */
+public class ExcludedService {
+    public String getExcludedMethod() {
+        return "Should be excluded";
+    }
+}
